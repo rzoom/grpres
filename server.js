2de7f16431f56e9fde3a465681f48c09a2518111
Rob Zumwalt <rob.zumwalt@gmail.com>
2013-09-21 13:29:05 -0500

    switching from Coffeescript to Javascript.

---
+++ server.js
@@ -0,0 +1,12 @@
+var reply = function(request, response) {
+response.writeHead(200, {
+    "Content-Type": "text/plain"
+});
+response.write("Hello there, World");
+return response.end();
+};
+
+var http = require("http");
+
+http.createServer(reply).listen(8888);
+

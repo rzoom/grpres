76cc3c121cb13464ba17f3039b37089d036f18c1
Rob Zumwalt <rob.zumwalt@gmail.com>
2013-09-21 22:02:33 -0500

    Clean up formatting

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+
+
 var reply = function(request, response) {
-response.writeHead(200, {
-    "Content-Type": "text/plain"
-});
-response.write("Hello there, World");
-return response.end();
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.write("Hello there, World");
+    return response.end();
 };
 
 var http = require("http");
